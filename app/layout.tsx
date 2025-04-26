6820b3fc70b1ffba20ac831fc8ac6552d3f0bdea
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-04-26 22:07:07 +0100

    feat: add og

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,21 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'DeepWiki Badge Generator',
   description: 'Generate badges for your DeepWiki from GitHub repositories',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://deepwiki.ryoppippi.com',
+    title: 'DeepWiki Badge Generator',
+    description: 'Generate badges for your DeepWiki from GitHub repositories',
+    images: `${process.env.CF_PAGES_URL}/screenshot.jpeg`,
+  },
+  twitter: {
+    title: 'DeepWiki Badge Generator',
+    description: 'Generate badges for your DeepWiki from GitHub repositories',
+    card: 'summary_large_image',
+    creator: '@ryoppippi',
+    images: `${process.env.CF_PAGES_URL}/screenshot.jpeg`,
+  }
 }
 
 export default function RootLayout({
