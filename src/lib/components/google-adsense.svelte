e71ab4ed23b26602726fc7002511c0f0ac650e93
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-06-08 22:37:31 +0100

    fix: google-adsense script

--- src/lib/components/google-adsense.svelte
+++ src/lib/components/google-adsense.svelte
@@ -1,20 +1,20 @@
 <script lang='ts'>
 	import { dev } from '$app/environment';
-	import { onMount } from 'svelte';
 
 	interface Props {
 		pId: string;
 	}
 
 	const { pId }: Props = $props();
-
-	onMount(() => {
-		if (!dev && pId) {
-			const script = document.createElement('script');
-			script.async = true;
-			script.crossOrigin = 'anonymous';
-			script.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}`;
-			document.head.appendChild(script);
-		}
-	});
 </script>
+
+<!-- eslint-disable svelte/html-self-closing  -->
+<svelte:head>
+	{#if !dev && pId}
+		<script
+			async
+			crossorigin='anonymous'
+			src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-{pId}'
+		></script>
+	{/if}
+</svelte:head>
